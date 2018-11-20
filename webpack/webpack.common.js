cd7fa59579b81538f034f6fce6fc90133ca0b274
hannd <hannd@oneempower.com>
2018-11-20 14:11:27 +0700

    f

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -96,7 +96,7 @@ module.exports = options => ({
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: `'${options.env}'`,
-        VERSION: `'${utils.parseVersion()}'`,
+        VERSION: `''`,
         DEBUG_INFO_ENABLED: options.env === 'development',
         // The root URL for API calls, ending with a '/' - for example: `"https://www.jhipster.tech:8081/myservice/"`.
         // If this URL is left empty (""), then it will be relative to the current context.
